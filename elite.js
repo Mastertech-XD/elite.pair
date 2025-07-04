0906280251a30d9b33a91450ee6de89d653306a4
Mastetech-XD <masterpeaceelite@gmail.com>
2025-07-04 07:56:52 +0300

    Update elite.js

--- elite.js
+++ elite.js
@@ -1,29 +1,46 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const path = require('path');
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/server', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/index.html')
-})
+
+const PORT = process.env.PORT || 5000;
+
+// Import routers
+const qrRouter = require('./qr');
+const pairRouter = require('./pair');
+
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use('/server', qrRouter);
+app.use('/code', pairRouter);
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTML routes
+app.get('/pair', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pair.html'));
+});
+
+app.get('/qr', (req, res) => {
+  res.sendFile(path.join(__dirname, 'qr.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(PORT, () => {
-    console.log(`
-Don't Forgot To Give Star ELITE-XD 
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
- Server running on http://localhost:` + PORT)
-})
-
-module.exports = app
+module.exports = app;
